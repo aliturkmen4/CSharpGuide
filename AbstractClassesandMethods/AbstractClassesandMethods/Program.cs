33ee71c2c76eb37f4ce219cdc1e8f0eb7cb424e7
Ali Türkmen <84051961+aliturkmen4@users.noreply.github.com>
2022-01-24 01:34:04 +0300

    Add files via upload

---
+++ AbstractClassesandMethods/AbstractClassesandMethods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesandMethods
+{
+    class Program
+    {
+        //abstract-->soyut
+        //abstract methodlar abstract sınıflar içinde kullanılır!
+        //doğrudan soyut class la ilgili obje oluşturamazsın!
+        static void Main(string[] args)
+        {
+            Cember c1 = new Cember();
+            c1.SekilCiz();
+            Console.ReadLine();
+        }
+    }
+    abstract class Sekil
+    {
+        //class'ın orjinalinde şekilçiz'i kullanmayıp kalıtılanlarda kullanacaksam abstract!
+        public abstract void SekilCiz(); //virtual yapınca içine doldurman lazım, abstract ta değil!
+    }
+    class Cember : Sekil
+    {
+        public override void SekilCiz() //abstract alırsan override etmek zorundasın!
+        {
+            Console.WriteLine("Çember çiziliyor!");
+        }
+    }
+}
