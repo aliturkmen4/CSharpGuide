771778c9439b850af5fbfade1b645ac0c80f8188
Ali Türkmen <84051961+aliturkmen4@users.noreply.github.com>
2022-01-24 01:34:27 +0300

    Add files via upload

---
+++ Constructor(Yapici)Method/Constructor(Yapici)Method/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Yapici_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Insan i1 = new Insan();
+            Insan i2 = new Insan("Ali Türkmen",25);          
+            i2.BilgileriSoyle();
+            Console.ReadLine();
+        }
+    }
+    class Insan
+    {
+        string isim;
+        int yas;
+
+        public Insan() //constructor method kısmı
+        {
+            Console.WriteLine("Yeni bir insan objesi oluşturuldu!");
+        }
+
+        public Insan(string name,int yas)
+        {
+            this.isim = name;
+            this.yas = yas;
+        }
+
+        public void BilgileriSoyle()
+        {
+            Console.WriteLine("Adım: {0}, Yaşım:{1}",isim,yas);
+        }
+    }
+}
