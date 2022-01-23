e5095d5de9af6b8cce096d15d877be6752ec2cb1
Ali Türkmen <84051961+aliturkmen4@users.noreply.github.com>
2022-01-24 01:42:35 +0300

    Add files via upload

---
+++ This-Readonly-Const/This-Readonly-Const/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace This_Readonly_Const
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Isci i1 = new Isci(2020.5);
+            Isci i2 = new Isci(4040.8);
+            Console.WriteLine(i1.maas+" "+i2.maas);
+            
+            //readonly constructor içerisinde değiştirilebilir!(consttan farkı)
+            /*readonly sabitine sonradan değer atanabilir!(consttan farkı)
+            const sabitinin değeri oluşturulduğu anda verilmek zorundadır!*/
+            //readonly matematiksel işlemlerin sonunda değerine kavuşabilir ama const ulaşamaz!
+            Console.ReadLine();
+        }
+    }
+    class Isci
+    {
+       public readonly double maas; //değiştirilemez.
+        public Isci(double salary)
+        {
+            this.maas = salary;
+        }
+        readonly double mutA = Math.Abs(-1);
+        //const double mutB = Math.Abs(-5);
+    }
+}
