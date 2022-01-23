7cebf41bb44570872943902f5a6459d7b78e4488
Ali Türkmen <84051961+aliturkmen4@users.noreply.github.com>
2022-01-24 01:40:48 +0300

    Add files via upload

---
+++ Polymorphism-Virtual-Override/Polymorphism-Virtual-Override/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Virtual_Override
+{
+    class Program
+    {
+        //polymorphism-->birden fazla formun oluşma durumu
+        static void Main(string[] args)
+        {
+            Sekil s1 = new Sekil();
+            Dikdortgen d1 = new Dikdortgen();
+            Cember c1 = new Cember();
+            c1.SekilCiz();
+            d1.SekilCiz();
+            s1.SekilCiz();
+            Console.ReadLine();
+        }
+    }
+    class Sekil
+    {
+        public virtual void SekilCiz() //virtual-->sanal //kalıtım alınacak classta uygulanır!
+        {
+            Console.WriteLine("Şekil çiziliyor.");
+        }
+    }
+    class Dikdortgen : Sekil
+    {
+        public override void SekilCiz() //override-->geçersiz kılmak //Override yaparak base classtaki şekil çiziliyor'u geçersiz kıldım!
+        {
+            Console.WriteLine("Didörtgen çiziliyor.");
+        }
+    }
+    class Cember : Sekil
+    {
+        public override void SekilCiz()
+        {
+            Console.WriteLine("Çember çiziliyor.");
+        }
+    }
+}
