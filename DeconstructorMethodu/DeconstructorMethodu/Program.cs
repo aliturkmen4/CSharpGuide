9fd75c184b55f84c0df302b5eea641532d20798d
Ali Türkmen <84051961+aliturkmen4@users.noreply.github.com>
2022-01-24 01:34:51 +0300

    Add files via upload

---
+++ DeconstructorMethodu/DeconstructorMethodu/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeconstructorMethodu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Insan i1 = new Insan();
+            ObjeleriOlustur();
+            GC.Collect(); // main içerisinde kullanılmayanları çöpe attım!(i2'yi)
+            Console.ReadLine();
+        }
+        static void ObjeleriOlustur()
+        {         
+            Insan i2 = new Insan();
+        }
+    }
+    class Insan
+    {
+        public string isim;
+
+        public Insan()
+        {
+            Console.WriteLine("İnsan oluşturuldu.");
+        }
+        ~Insan()
+        {
+            Console.WriteLine("Obje yok oldu!"); 
+        }   
+    }
+}
